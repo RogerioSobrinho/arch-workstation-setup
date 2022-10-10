e26f93d6165a8ee4f7b5fa55d996bf8cd79c04ae
Rogerio Sobrinho <github@rogeriosobrinho.me>
2022-10-10 20:02:25 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -292,7 +292,7 @@ arch-chroot /mnt chown -R $username:$username /home/${username}/.config
 [ -n "$username" ] && echo "Setting user password for ${username}." && arch-chroot /mnt /bin/passwd "$username"
 
 # Giving wheel user sudo access.
-sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /mnt/etc/sudoers
+sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /mnt/etc/sudoers
 
 # Change audit logging group
 echo "log_group = audit" >> /etc/audit/auditd.conf
